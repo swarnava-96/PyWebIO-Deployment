3011cd36ba8843ef7c5b2d3e2b0d945bba9ccd56
Swarnava Mukherjee <75041273+swarnava-96@users.noreply.github.com>
2021-06-27 02:45:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 27 02:30:59 2021
+
+@author: SWARNAVA
+"""
+
+# A simple script to do MAthematical calaculation
+from pywebio.input import *
+from pywebio.output import *
+def mathemticaloperation():
+    a = input("Enter the first number：", type=FLOAT)
+    b = input("Enter the second number：", type=FLOAT)
+    c=0
+    operation = radio("Choose one operation", options=['+', '*', '/', '%'])
+    operation_name=""
+    if operation=="+":
+        operation_name="Addition"
+        c=a+b
+    elif operation=="*":
+        operation_name="Multiplication"
+        c=a*b
+    elif operation=="/":
+        operation_name="Division"
+        c=a/b
+    else:
+        operation_name="Modulus"
+        c=a%b
+        
+    put_text('The operation selected is: %s. and the output is: %.1f' % (operation_name, c))
+if __name__ == '__main__':
+    mathemticaloperation()
